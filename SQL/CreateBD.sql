280589b2f0164d73b67424ca6200960d96921fac
Julie Choquet <julie.choquet@ensiie.fr>
2016-04-29 21:58:14 +0200

    script creation base de donnee avec changements

--- SQL/CreateBD.sql
+++ SQL/CreateBD.sql
@@ -1,106 +1,104 @@
-DROP TABLE Utilisateur;
-DROP TABLE Photo;
-DROP TABLE Concours;
-DROP TABLE Compteur;
-DROP TABLE Soiree;
-DROP TABLE Commentaire;
+DROP TABLE utilisateur CASCADE;
+DROP TABLE photo CASCADE;
+DROP TABLE concours CASCADE;
+DROP TABLE compteur CASCADE;
+DROP TABLE soiree CASCADE;
+DROP TABLE commentaire CASCADE;
+DROP TABLE identification CASCADE;
+DROP TABLE vote CASCADE;
 
 
-CREATE TABLE Utilisateur (
-Identifiant VARCHAR (12) PRIMARY KEY,
-Mdp VARCHAR (20) ,
-Nom VARCHAR (20),
-Prenom VARCHAR (20),
-Promo INTEGER,
-Surnom VARCHAR (20),
-Quote VARCHAR (255));
+CREATE TABLE utilisateur (
+identifiant VARCHAR (12) PRIMARY KEY,
+mdp VARCHAR (20) ,
+nom VARCHAR (20),
+prenom VARCHAR (20),
+promo INTEGER,
+surnom VARCHAR (20),
+quote VARCHAR (255),
+avatar VARCHAR (3));
 
 
 
-CREATE TABLE Soiree (
-IdSoiree INTEGER PRIMARY KEY,
-Theme Varchar (20),
-Annee DATE,
-Description VARCHAR (300));
+CREATE TABLE soiree (
+idsoiree SERIAL PRIMARY KEY,
+theme VARCHAR (20),
+description VARCHAR (300),
+annee INTEGER,
+d DATE);
 
 
-CREATE TABLE Concours (
-IdConcours INTEGER PRIMARY KEY,
-Nom VARCHAR (20),
-Description VARCHAR(300),
-Winner INTEGER,
-EnCours BOOLEAN);
+CREATE TABLE concours (
+idconcours SERIAL PRIMARY KEY,
+nom VARCHAR (20),
+description VARCHAR(300),
+winner INTEGER,
+encours BOOLEAN);
 
-CREATE TABLE Photo (
-IdPhoto INTEGER PRIMARY KEY,
-IdSoiree INTEGER,
-Date_MeL DATE,
-Heure TIME,
-ComPosteur VARCHAR (255),
-NbLike INTEGER,
-IdPosteur VARCHAR(12),
+CREATE TABLE photo (
+idphoto SERIAL PRIMARY KEY,
+idsoiree INTEGER,
+date_mel DATE,
+heure TIME,
+composteur VARCHAR (255),
+nblike INTEGER,
+idposteur VARCHAR(12),
+extension VARCHAR(3),
+heure_mel TIME,
 
-CONSTRAINT lien_soiree FOREIGN KEY (IdSoiree) REFERENCES Soiree(IdSoiree),
-CONSTRAINT lien_posteur FOREIGN KEY (IdPosteur) REFERENCES Utilisateur(Identifiant)
+CONSTRAINT lien_soiree FOREIGN KEY (idsoiree) REFERENCES soiree(idsoiree),
+CONSTRAINT lien_posteur FOREIGN KEY (idposteur) REFERENCES utilisateur(identifiant)
 );
 
 
-CREATE TABLE Identification (
-IdPhoto INTEGER,
-IdUtilisateur VARCHAR(12),
+CREATE TABLE identification (
+idphoto INTEGER,
+idutilisateur VARCHAR(12),
 
-CONSTRAINT lien_photo FOREIGN KEY (IdPhoto) REFERENCES Photo(IdPhoto),
-CONSTRAINT lien_identifie FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateur(Identifiant)
+CONSTRAINT lien_photo FOREIGN KEY (idphoto) REFERENCES photo(idphoto),
+CONSTRAINT lien_identifie FOREIGN KEY (idutilisateur) REFERENCES utilisateur(identifiant)
 );
 
 
 
-
-
-
-
-
-CREATE TABLE Commentaire (
-IdCom INTEGER PRIMARY KEY,
-IdPhoto INTEGER ,
-IdAuteur VARCHAR (12),
-Date_Post DATE,
+CREATE TABLE commentaire (
+idcom SERIAL PRIMARY KEY,
+idphoto INTEGER ,
+idauteur VARCHAR (12),
+date_post DATE,
+heure_post TIME,
 contenu VARCHAR (255),
 
-CONSTRAINT lien_photo FOREIGN KEY (IdPhoto) REFERENCES Photo(IdPhoto),
-CONSTRAINT lien_auteur FOREIGN KEY (IdAuteur) REFERENCES Utilisateur(Identifiant)
+CONSTRAINT lien_photo FOREIGN KEY (idphoto) REFERENCES photo(idphoto),
+CONSTRAINT lien_auteur FOREIGN KEY (idauteur) REFERENCES utilisateur(identifiant)
 );
 
 
-
-
-CREATE TABLE Compteur (
-IdPhoto INTEGER,
-IdConcours INTEGER,
+CREATE TABLE compteur (
+idphoto INTEGER,
+idconcours INTEGER,
 nbre_votes INTEGER,
 
-CONSTRAINT lien_photo FOREIGN KEY (IdPhoto) REFERENCES Photo(IdPhoto),
-CONSTRAINT lien_concours FOREIGN KEY (IdConcours) REFERENCES Concours(IdConcours)
+CONSTRAINT lien_photo FOREIGN KEY (idphoto) REFERENCES photo(idphoto),
+CONSTRAINT lien_concours FOREIGN KEY (idconcours) REFERENCES concours(idconcours)
 );
 
 
 
 
-CREATE TABLE Vote (
-IdPhoto INTEGER,
-IdUtilisateur VARCHAR(12),
-IdConcours INTEGER,
+CREATE TABLE vote (
+idphoto INTEGER,
+idutilisateur VARCHAR(12),
+idconcours INTEGER,
 
 
-CONSTRAINT lien_concours FOREIGN KEY (IdConcours) REFERENCES Concours(IdConcours),
-CONSTRAINT lien_photo FOREIGN KEY (IdPhoto) REFERENCES Photo(IdPhoto),
-CONSTRAINT lien_utilisateur FOREIGN KEY (IdUtilisateur) REFERENCES Utilisateur(Identifiant)
+CONSTRAINT lien_concours FOREIGN KEY (idconcours) REFERENCES concours(idconcours),
+CONSTRAINT lien_photo FOREIGN KEY (idphoto) REFERENCES photo(idphoto),
+CONSTRAINT lien_utilisateur FOREIGN KEY (idutilisateur) REFERENCES utilisateur(identifiant)
 
 );
 
 
-CREATE SEQUENCE photo_id_seq;
-ALTER TABLE photo ALTER idphoto SET DEFAULT NEXTVAL('photo_id_seq');
+INSERT INTO utilisateur(identifiant, mdp, nom, prenom, promo, surnom, quote, avatar) VALUES ('administrat', 'rootroot', 'norris', 'chuck', '2000', '', '', '');
 
-CREATE SEQUENCE soiree_id_seq;
-ALTER TABLE soiree ALTER idsoiree SET DEFAULT NEXTVAL('soiree_id_seq');
+
