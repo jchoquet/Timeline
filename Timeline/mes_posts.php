0b63289ea38b3b4667d0c7754446628974a2b2e4
Julien NATY-DAUFIN <julien.naty-daufin@ensiie.fr>
2016-05-02 19:51:35 +0200

    Affichage de Mes Posts

--- Timeline/mes_posts.php
+++ Timeline/mes_posts.php
@@ -37,7 +37,7 @@
       
       <div class="container-fluid">
 
-            <?php carousel("pgsql:host=localhost;dbname=projet_web", "SELECT IdPhoto FROM Photo WHERE IdPosteur=$_SESSION['login']); ?>      /* a verifier les arguments!! */
+            <?php carousel("pgsql:host=localhost;dbname=projet_web", "SELECT idphoto FROM photo WHERE idposteur=$_SESSION['login']); ?>      /* a verifier les arguments!! */
 
       </div>
 	
