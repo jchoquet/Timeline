15218f80f7eeb74ee435ca656e2c84a58e93bdf7
Julie Choquet <julie.choquet@ensiie.fr>
2016-05-09 00:44:54 +0200

    tunning du modal + supp trash

--- Timeline/admin_gestion.php
+++ Timeline/admin_gestion.php
@@ -46,11 +46,19 @@
 	 	<h3 class="page-header"> Gestion administrateur </h3>
 
 	 	<div class="container-fluid">
-	 		<ul>
-	 			<li><a href="admin_add_soiree.php">Ajout d'une soirée</a></li>
-	 			<li><a href="admin_del_soiree.php">Suppression d'une soirée</a></li>
-	 		</ul>
+    <div class="row">
+          <div class="col-xs-12" id="nav">
+    	 		<ul>
+    	 			<li><span class="glyphicon glyphicon-plus-sign"></span><a href="admin_add_soiree.php">Ajout d'une soirée</a></li>
+    	 			<li><span class="glyphicon glyphicon-minus-sign"></span><a href="admin_del_soiree.php">Suppression d'une soirée</a></li>
+    	 		</ul>
+          <ul>
+            <li><span class="glyphicon glyphicon-play"></span><a href="admin_add_concours.php">Lancer un concours</a></li>
+            <li><span class="glyphicon glyphicon-stop"></span><a href="admin_stop_concours.php">Stopper un concours</a></li>
+          </ul>
+          </div>
+    </div>
 	 	</div>
-   
+  
 
  <?php include 'footer.php'; ?>
--- Timeline/mes_posts.php
+++ Timeline/mes_posts.php
@@ -65,6 +65,7 @@
    <link rel="stylesheet" href="css/menu.css">
    <link rel="stylesheet" href="css/mes_posts.css">
    <link rel="stylesheet" type="text/css" href="css/modal.css">
+  <link rel="stylesheet" type="text/css" href="css/buttonLink.css">
 
 </head>
 
